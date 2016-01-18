f0ae8fc5e7403c7a1e8d9054eed412341750d280
Pavel N <nikitinskypavel@gmail.com>
2016-01-18 20:55:43 +0200

    second

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,11 @@
 class Order < ActiveRecord::Base
+  validates :sum, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 600 }
+  validates :days, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 30 }
+  validates_presence_of  :email,:product_id,:mobile,:sum,:days
+  validates :email,:product_id,:mobile, length: { maximum: 50 }
+  validates :product_id, length: { maximum: 500 }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/,  message: "Not valid"}
+
+  validates :mobile, format: {:with => /!((^[XYZ]\d{7,8}|^\d{8})[A-HJ-NP-TV-Z]$)/,  :message => 'Not valid'}
+
 end
--- db/migrate/20160118170203_create_orders.rb
+++ db/migrate/20160118170203_create_orders.rb
@@ -1,7 +1,11 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-
+      t.string :email
+      t.string :product_id
+      t.string :mobile
+      t.integer  :sum
+      t.integer  :days
       t.timestamps null: false
     end
   end
